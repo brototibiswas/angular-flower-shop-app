0f50c327152bd92ca7b7c492bbb1db5759fcef43
Brototi Biswas <72146916+brototibiswas@users.noreply.github.com>
2023-11-05 13:40:50 -0500

    add basic ui form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputFormComponent } from './component/input-form/input-form.component';
+import { FormInputComponent } from './component/input/form-input/form-input.component';
+import { PrimaryButtonComponent } from './component/button/primary-button/primary-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFormComponent,
+    FormInputComponent,
+    PrimaryButtonComponent
   ],
   imports: [
     BrowserModule
