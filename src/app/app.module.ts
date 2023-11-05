d25ce35952749ab4d14d28db531e55ee4803a18b
Brototi Biswas <72146916+brototibiswas@users.noreply.github.com>
2023-11-05 16:06:51 -0500

    add simple workable form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { InputFormComponent } from './component/input-form/input-form.component';
 import { FormInputComponent } from './component/input/form-input/form-input.component';
 import { PrimaryButtonComponent } from './component/button/primary-button/primary-button.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputFormComponent,
     FormInputComponent,
-    PrimaryButtonComponent
+    PrimaryButtonComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/component/input-form/input-form.component.ts
+++ src/app/component/input-form/input-form.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpResponse,
+} from '@angular/common/http';
+import { Component, QueryList } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { FormInputComponent } from '../input/form-input/form-input.component';
 
 @Component({
   selector: 'app-input-form',
   templateUrl: './input-form.component.html',
-  styleUrls: ['./input-form.component.scss']
+  styleUrls: ['./input-form.component.scss'],
 })
 export class InputFormComponent {
+  errorMsg: string = '';
+  formData!: QueryList<FormInputComponent>;
 
+  nameField!: string;
+  priceField!: number;
+  unitField!: number;
+
+  constructor(private http: HttpClient) {}
+
+  async onFormSubmit() {
+    const body = {
+      name: this.nameField,
+      price: this.priceField,
+      units: this.unitField,
+    };
+    console.log('submit form', body);
+    this.submitForm(body);
+  }
+
+  async submitForm(data: any) {
+    try {
+      const resp = await lastValueFrom(
+        this.http.post('http://localhost:3000/submit-form', data)
+      );
+      console.log(resp);
+    } catch (error) {
+      console.log(error);
+      if (error instanceof HttpErrorResponse) {
+        this.errorMsg = error.error.message;
+      }
+    }
+  }
 }
